f96b292ed6857c3c29bc1c7d3a31f428b37634c1
Jiun-Ting <R03323057@ntu.edu.tw>
2020-07-14 11:37:39 -0500

    v0

---
+++ 463 - Island Perimeter/463 - Island Perimeter_v0.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m, n = len(grid), len(grid[0])
+        count = 0
+        conn = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    count += 1
+                    for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
+                        if  0 <= x < m and 0 <= y < n and grid[x][y] == 1:
+                            conn += 1
+        return 4*count-conn
---
+++ 463 - Island Perimeter/README.md
@@ -0,0 +1,10 @@
+Problem: https://leetcode.com/problems/island-perimeter/
+v0:
+Strategy: Count number of 1 and number of connecting edges
+
+Time Complexity: O(N), where N is the length of input list
+Space Complexity: O(1)
+
+result:
+Runtime: 556 ms, faster than 70.96% of Python online submissions for Island Perimeter.
+Memory Usage: 13.1 MB, less than 36.62% of Python online submissions for Island Perimeter.
