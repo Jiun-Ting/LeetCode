61565f702f9fc3ecee7fdff6d75dd139127a61b5
Jiun-Ting <R03323057@ntu.edu.tw>
2020-07-16 10:40:29 -0500

    Update and rename 692 - Top K Frequent Words_v1 to 692 - Top K Frequent Words_v1.py

--- 692 - Top K Frequent Words/692 - Top K Frequent Words_v1.py
+++ 692 - Top K Frequent Words/692 - Top K Frequent Words_v1.py
@@ -6,7 +6,6 @@ class Solution(object):
         :rtype: List[str]
         """
         count = collections.Counter(words)
-
         s = heapq.nsmallest(k, count.items(), key=lambda x:(-x[1], x[0]))
 
         return [i[0] for i in s]
