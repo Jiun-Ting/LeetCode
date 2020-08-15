e0707c2c9ce9f5ed58dddcb96b681b66cb502ce3
Jiun-Ting <R03323057@ntu.edu.tw>
2020-08-15 15:33:57 -0500

    v0

---
+++ 35 - Search Insert Position/35 - Search Insert Position_v0.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l, r = 0, len(nums)
+        while l < r:
+            mid = (l + r)//2
+            if nums[mid] >= target:
+                r = mid
+            else:
+                l = mid + 1
+        return l
---
+++ 35 - Search Insert Position/README.md
@@ -0,0 +1,9 @@
+Problem: Search Insert Position
+Strategy: Binary Search
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+result:
+Runtime: 32 ms, faster than 94.48% of Python online submissions for Search Insert Position.
+Memory Usage: 13.3 MB, less than 33.07% of Python online submissions for Search Insert Position.
