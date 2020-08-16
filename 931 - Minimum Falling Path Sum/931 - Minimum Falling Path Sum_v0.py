8e4649e93c9e1b8d1fd2abbd86a724502e4e471c
Jiun-Ting <R03323057@ntu.edu.tw>
2020-08-16 14:09:05 -0500

    v0

---
+++ 931 - Minimum Falling Path Sum/931 - Minimum Falling Path Sum_v0.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def minFallingPathSum(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: int
+        """
+        n = len(A)
+        dp = [[0 for i in range(n)] for j in range(n)]
+        for i in range(n):
+            dp[0][i] = A[0][i]
+
+        for i in range(1, n):
+            for j in range(n):
+                if j == 0:
+                    dp[i][j] = A[i][j] + min(dp[i-1][j], dp[i-1][j+1])
+                elif j == n-1:
+                    dp[i][j] = A[i][j] + min(dp[i-1][j], dp[i-1][j-1])
+                else:
+                    dp[i][j] = A[i][j] + min(dp[i-1][j], dp[i-1][j+1], dp[i-1][j-1])
+        print(dp)
+        return min(dp[n-1][i] for i in range(n))
+        
---
+++ 931 - Minimum Falling Path Sum/README.md
@@ -0,0 +1,9 @@
+Problem: https://leetcode.com/problems/minimum-falling-path-sum/
+Strategy: DP
+
+Time Complexity: O(n^2)
+Space Complexity: O(n^2)
+
+result:
+Runtime: 96 ms, faster than 86.35% of Python online submissions for Minimum Falling Path Sum.
+Memory Usage: 13.2 MB, less than 69.37% of Python online submissions for Minimum Falling Path Sum.
