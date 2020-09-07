69d14a6038539813241a097a9e0f9e232ca9ac2e
Jiun-Ting <R03323057@ntu.edu.tw>
2020-09-07 12:49:26 -0500

    DP

---
+++ 377. Combination Sum IV/377. Combination Sum IV_v0.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def combinationSum4(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        dp = [0]*(target+1)
+        dp[0] = 1
+        for i in range(target+1):
+            for j in nums:
+                if i-j >= 0:
+                    dp[i] += dp[i-j]
+
+        return dp[target]
